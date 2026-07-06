16b5a052c20f9c2952352aef6202e85130aff35a
gomezm03 <110982945+gomezm03@users.noreply.github.com>
2026-07-06 17:58:35 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Bump CACHE_VERSION whenever you deploy changed files so clients
    pick up the new build on their next load. */
 
-const CACHE_VERSION = "chatterx-v2";
+const CACHE_VERSION = "chatterx-v3";
 
 const ASSETS = [
   "./",
